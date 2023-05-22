f4c1d2262674fcb8ce5595fe94de319292f31940
A Stone <astone@heckgrammar.co.uk>
2023-05-22 11:02:56 +0100

    Add project files.

---
+++ 3.2.4RelationalOperations/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._2._4RelationalOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // equal to
+            int a = 1;
+            if(a == 1)
+            {
+                Console.WriteLine("Equal to");
+            }
+            // not equal to
+            if(a != 1)
+            {
+                Console.WriteLine("Not equal to");
+            }
+            // less than
+            if (a < 3)
+            {
+                Console.WriteLine("Less than");
+            }
+            // greater than
+            if (a > 0)
+            {
+                Console.WriteLine("Greater than");
+            }
+            // less than or equal to
+            if (a <= 2)
+            {
+                Console.WriteLine("Less than or equal to");
+            }
+            // greater than or equal to.
+            if(a >= 0)
+            {
+                Console.WriteLine("greter than or equal to");
+            }
+        }
+    }
+}
